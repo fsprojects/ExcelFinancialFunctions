9abc06311a61a7db7e0765d62207446d7a995670
James Coliz <jcoliz@outlook.com>
2021-11-24 17:05:51 -0800

    Experiment with (hopefully) proper link format for generated docs

--- src/ExcelFinancialFunctions/wrapperdotnettype.fs
+++ src/ExcelFinancialFunctions/wrapperdotnettype.fs
@@ -18,7 +18,8 @@ open Excel.FinancialFunctions.OddBonds
 
 /// A wrapper class to expose the Excel financial functions API to .NET clients
 type Financial =
-    /// The accrued interest for a security that pays periodic interest ([learn more](http://office.microsoft.com/en-us/excel/HP052089791033.aspx))
+    /// The accrued interest for a security that pays periodic interest
+    /// <seealso href="https://support.microsoft.com/en-us/office/accrint-function-fe45d089-6722-4fb3-9379-e1f911d8dc74">ACCRINT function</seealso>
     static member AccrInt (issue, firstInterest, settlement, rate, par, frequency, basis, calcMethod) =
         calcAccrInt issue firstInterest settlement rate par frequency basis calcMethod
     /// The accrued interest for a security that pays periodic interest ([learn more](http://office.microsoft.com/en-us/excel/HP052089791033.aspx))
